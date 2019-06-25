c24187102fbac1b34617b2b41d2124a934dbd508
Andrew Perrin <andrew.perrin@cgi.com>
2019-06-25 16:09:59 -0400

    experiment

--- src/com/perrin/jenkinsUtils/nexus.groovy
+++ src/com/perrin/jenkinsUtils/nexus.groovy
@@ -9,19 +9,23 @@ import org.apache.http.client.methods.*
 import org.apache.http.entity.*
 import org.apache.http.impl.client.*
 
-  // def search(keyword,repository){
-def search(){
-  // def url = "http://192.168.33.10:8081/service/rest/v1/searc?sort=version&direction=desc&q=${keyword}&repository=${repository}"
-  def url = "http://192.168.33.10:8081/service/rest/v1/search?sort=version&direction=desc&q=jquery"
-  def get = new HttpGet(url)
-  get.addHeader("content-type", "application/json")
+def search(keyword,repository){
+  def url = "http://192.168.33.10:8081/service/rest/v1/searc?sort=version&direction=desc&q=${keyword}&repository=${repository}"
 
-  def client = HttpClientBuilder.create().build()
-  def response = client.execute(get)
+  HttpClientBuilder.create().build().withCloseable { client -> 
+    final request = new HttpGet(url)
+    request.addHeader("content-type", "application/json")
 
-  def bufferedReader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()))
-  def jsonResponse = bufferedReader.getText()
-  println "response: \n" + jsonResponse
+    client.execute(request).withCloseable{ response ->
+      if (response.statusLine.statusCode != 200){
+        println "Error from server: ${response.statusLine.statusCode}"
+      }
+
+      def bufferedReader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()))
+      def jsonResponse = bufferedReader.getText()
+      println "response: \n" + jsonResponse
+    }
+  }
 }
 
 return this;
