1d1439297fe92c08fef35cce338df2e9fa6d6504
Andrew Perrin <andrew.perrin@cgi.com>
2019-06-25 13:06:47 -0400

    Inital commit

---
+++ src/com/perrin/jenkins-utils/nexus-search.groovy
@@ -0,0 +1,23 @@
+#!/usr/bin/groovy
+package com.perrin.jenkins-utils;
+
+@Grab(group='org.apache.httpcomponents', module='httpclient', version='4.5.2')
+
+import groovy.json.*
+
+import org.apache.http.client.methods.*
+import org.apache.http.entity.*
+import org.apache.http.impl.client.*
+
+def search(keyword,repository){
+  def url = "http://192.168.33.10:8081/service/rest/v1/searc?sort=version&direction=desc&q=${keyword}&repository=${repository}"
+  def get = new HttpGet(url)
+  get.addHeader("content-type", "application/json")
+
+  def client = HttpClientBuilder.create().build()
+  def response = client.execute(get)
+
+  def bufferedReader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()))
+  def jsonResponse = bufferedReader.getText()
+  return jsonResponse;
+}
